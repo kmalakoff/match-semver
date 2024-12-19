21b189d3a9eced35f7c0f8618f9c19087d37704e
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-19 08:21:42 -0800

    Upgrade modules

---
+++ dist/cjs/index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return match;
+    }
+});
+var _semver = /*#__PURE__*/ _interop_require_default(require("semver"));
+function _interop_require_default(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+function match(version, comparators) {
+    if (comparators.eq && !_semver.default.eq(version, comparators.eq) || comparators.lt && !_semver.default.lt(version, comparators.lt) || comparators.lte && !_semver.default.lte(version, comparators.lte) || comparators.gt && !_semver.default.gt(version, comparators.gt) || comparators.gte && !_semver.default.gte(version, comparators.gte)) {
+        return false;
+    }
+    return true;
+}
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
