195deaa030ef23b5138805f911f7cf50d9118098
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 12:14:03 -0700

    Comprehensive types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import semver from 'semver';
 
-export default function match(version, comparators) {
+export interface Comparators {
+  eq?: string;
+  lt?: string;
+  lte?: string;
+  gt?: string;
+  gte?: string;
+}
+
+export default function match(version: string, comparators: Comparators): boolean {
   if ((comparators.eq && !semver.eq(version, comparators.eq)) || (comparators.lt && !semver.lt(version, comparators.lt)) || (comparators.lte && !semver.lte(version, comparators.lte)) || (comparators.gt && !semver.gt(version, comparators.gt)) || (comparators.gte && !semver.gte(version, comparators.gte))) {
     return false;
   }
